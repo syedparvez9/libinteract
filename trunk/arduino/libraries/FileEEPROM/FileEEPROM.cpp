d034ea0548d1dd33172a886057c6f10b92ce4376
info@tats.name <info@tats.name@5f32af66-d647-0410-8b61-d14924473623>
2011-08-18 20:21:12 +0000

    r1016@TatsLab:  tats | 2011-06-17 18:10:26 -0400  New library: FileEEPROM

---
+++ trunk/arduino/libraries/FileEEPROM/FileEEPROM.cpp
@@ -0,0 +1,40 @@
+#include "FileEEPROM.h"
+#include <avr/eeprom.h>
+
+// Read something.
+int MyFileEEPROM::read(void *ptr, int blockSize, int nBlocks) {
+  nBlocks *= blockSize; // nBlocks becomes total memory (saves one int)
+  if (pos + nBlocks > EEPROM_SIZE)
+    return 0; // TODO: rendre compliant avec fread
+  eeprom_read_block (ptr, (const void*) pos, nBlocks);
+  pos += nBlocks;
+}
+
+// Write.
+int MyFileEEPROM::write(void *ptr, int blockSize, int nBlocks) {
+  nBlocks *= blockSize; // nBlocks becomes total memory (saves one int)
+  if (pos + nBlocks > EEPROM_SIZE)
+    return 0; // TODO: rendre compliant avec fread
+  eeprom_write_block (ptr, (void*) pos, nBlocks);
+  pos += nBlocks;
+}
+
+int MyFileEEPROM::eof() {
+  return (pos >= EEPROM_SIZE);
+}
+
+int MyFileEEPROM::seek(int offset/*, int whence*/) {
+  pos = offset;
+}
+
+// Tell where I am.
+int MyFileEEPROM::tell() {
+  return pos;
+}
+
+// Rewind to the start
+void MyFileEEPROM::rewind() {
+  pos = 0;
+}
+
+MyFileEEPROM FileEEPROM;
