846c85c433175ebf4182364d2ad553dd214af9cb
info@tats.name <info@tats.name@5f32af66-d647-0410-8b61-d14924473623>
2011-08-19 19:33:27 +0000

    r1036@TatsLab:  tats | 2011-08-19 15:33:22 -0400  Added some comments + cleaned up a bit

--- trunk/trunk/arduino/libraries/SeekableStream/SeekableStream.h
+++ trunk/trunk/arduino/libraries/SeekableStream/SeekableStream.h
@@ -1,6 +1,12 @@
 /*
  * SeekableStream.h
  *
+ * This class adds seek/peek functionalities to Stream. It is useful for addressable
+ * memories such as EEPROM, dataflash or SD cards.
+ *
+ * The idea was introduced in the following forum post:
+ * http://arduino.cc/forum/index.php/topic,69584.0.html
+ *
  * (c) 2011 Sofian Audry -- info(@)sofianaudry(.)com
  *
  * This program is free software: you can redistribute it and/or modify
@@ -33,17 +39,13 @@ class SeekableStream : public Stream
 public:
   virtual unsigned long tell() = 0;
   virtual void seek(unsigned long pos/*, int dir = CUR*/) = 0;
+  virtual void rewind() { seek(0); }
+
+  // NOTE: These methods should acually go in Stream.
   virtual bool eof() = 0;
-
-  virtual void flushInput() {
-    flush();
-  }
-
+  virtual void flushInput() { flush(); }
   virtual void flushOutput() = 0;
 
-  virtual void rewind() {
-    seek(0); // default
-  }
 };
 
 #endif /* RANDOMACCESSSTREAM_H_ */
