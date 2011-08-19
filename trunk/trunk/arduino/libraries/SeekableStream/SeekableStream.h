907e8fbb85e42f55722f7779e1ebaabb2c5d782f
info@tats.name <info@tats.name@5f32af66-d647-0410-8b61-d14924473623>
2011-08-19 17:27:19 +0000

    r1026@TatsLab:  tats | 2011-08-19 13:25:27 -0400  Added new SeekableStream API

---
+++ trunk/trunk/arduino/libraries/SeekableStream/SeekableStream.h
@@ -0,0 +1,49 @@
+/*
+ * SeekableStream.h
+ *
+ * (c) 2011 Sofian Audry -- info(@)sofianaudry(.)com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SEEKABLESTREAM_H_
+#define SEEKABLESTREAM_H_
+
+#include "Stream.h"
+
+/*
+#define BEG 0x0
+#define CUR 0x1
+#define END 0x2
+*/
+
+class SeekableStream : public Stream
+{
+public:
+  virtual unsigned long tell() = 0;
+  virtual void seek(unsigned long pos/*, int dir = CUR*/) = 0;
+  virtual bool eof() = 0;
+
+  virtual void flushInput() {
+    flush();
+  }
+
+  virtual void flushOutput() = 0;
+
+  virtual void rewind() {
+    seek(0); // default
+  }
+};
+
+#endif /* RANDOMACCESSSTREAM_H_ */
