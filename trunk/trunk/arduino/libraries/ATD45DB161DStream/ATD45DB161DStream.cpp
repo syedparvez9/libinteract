fb23be943673b737bb2021a77684df71c288849f
info@tats.name <info@tats.name@5f32af66-d647-0410-8b61-d14924473623>
2011-08-19 17:27:28 +0000

    r1027@TatsLab:  tats | 2011-08-19 13:27:14 -0400  Created first version of SeekableSgtream for dataflash ATD45DB161D

---
+++ trunk/trunk/arduino/libraries/ATD45DB161DStream/ATD45DB161DStream.cpp
@@ -0,0 +1,115 @@
+/*
+ * ATD45DB161DStream.cpp
+ *
+ * (c) 2011 Sofian Audry -- info(@)sofianaudry(.)com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ATD45DB161DStream.h"
+
+ATD45DB161DStream::ATD45DB161DStream() :
+  curr(0), outputBufferEmpty(true)
+{
+}
+
+void ATD45DB161DStream::begin(uint8_t csPin, uint8_t resetPin, uint8_t wpPin) {
+  /* Make sure SPI is initialized */
+  SPI.begin();
+
+  /* Let's wait 1 second, allowing use to press the serial monitor button :p */
+  delay(1000);
+
+  /* Initialize dataflash */
+  ATD45DB161D::begin(csPin, resetPin, wpPin);
+}
+
+void ATD45DB161DStream::write(uint8_t x) {
+  // End of page check.
+
+  /* Set dataflash so that any call to spi_tranfer will write the byte
+   * given as argument to the Buffer 1 */
+  BufferWrite(1, currentOffset());
+//  // Switch check.
+//  else if (!writeMode) {
+//    // Because we have previously switched to read mode, we now need to re-read the buffer.
+//
+//    writeMode = true;
+//  }
+
+  // Transfer data to buffer.
+  SPI.transfer(x);
+  outputBufferEmpty = false;
+
+  if (currentOffset() == ATD45DB161D_BYTES_PER_PAGE-1) {
+    // Send buffer to dataflash.
+    flushOutput();
+
+    // Fill buffer with current data from the page in order to preserve it.
+    // TODO: make sure we're doing this right
+    BufferRead(1, 0, 1); // TODO: why low speed (third arg) ?
+  }
+  curr++;
+}
+
+// From Stream.h
+int ATD45DB161DStream::available() {
+  // there's actually much more available but we're limited by the library (int)
+  return (int) min(ATD45DB161D_SIZE - curr, 32767);
+}
+
+int ATD45DB161DStream::read() {
+  int val = peek();
+  curr++;
+  return val;
+}
+
+int ATD45DB161DStream::peek() {
+  flushOutput();
+  ReadMainMemoryPage(currentPage(), currentOffset());
+  return SPI.transfer(0xff);
+}
+
+void ATD45DB161DStream::flush() {
+  // Nothing to do here.
+}
+
+void ATD45DB161DStream::flushOutput() {
+  if (!outputBufferEmpty) {
+    Serial3.print("Flushing to page: "); Serial3.println(currentPage());
+    BufferToPage(1, currentPage(), 1);
+    outputBufferEmpty = true;
+  }
+}
+
+unsigned long ATD45DB161DStream::tell() {
+  return curr;
+}
+
+void ATD45DB161DStream::seek(unsigned long pos/*, int dir = CUR*/) {
+  flushOutput(); // Flush buffer.
+  curr = min(pos, ATD45DB161D_SIZE); // cannot seek over max size
+}
+
+bool ATD45DB161DStream::eof() {
+  return (curr >= ATD45DB161D_SIZE);
+}
+
+int ATD45DB161DStream::currentPage() const {
+  return (curr / ATD45DB161D_BYTES_PER_PAGE);
+}
+
+int ATD45DB161DStream::currentOffset() const {
+  return (curr % ATD45DB161D_BYTES_PER_PAGE);
+}
---
+++ trunk/trunk/arduino/libraries/ATD45DB161DStream/ATD45DB161DStream.h
@@ -0,0 +1,62 @@
+/*
+ * ATD45DB161DStream.h
+ *
+ * (c) 2011 Sofian Audry -- info(@)sofianaudry(.)com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef AT45DB161DSTREAM_H_
+#define AT45DB161DSTREAM_H_
+
+#include "SeekableStream.h"
+#include "at45db161d.h"
+
+#define ATD45DB161D_BYTES_PER_PAGE 528
+#define ATD45DB161D_N_PAGES        4096
+#define ATD45DB161D_SIZE           2162688L // = BYTES_PER_PAGE * N_PAGES
+
+class ATD45DB161DStream : public ATD45DB161D, public SeekableStream {
+public:
+  unsigned long curr;
+  unsigned long bufferPage;
+  bool outputBufferEmpty;
+//  bool writeMode;
+
+  ATD45DB161DStream();
+
+  void begin(uint8_t csPin=SLAVESELECT, uint8_t resetPin=RESET, uint8_t wpPin=WP);
+//  void end();
+
+  // From Print.h
+  virtual void write(uint8_t x);
+
+  // From Stream.h
+  virtual int available();
+  virtual int read();
+  virtual int peek();
+  virtual void flush();
+
+  virtual void flushOutput();
+
+  // From SeekableStream.h
+  virtual unsigned long tell();
+  virtual void seek(unsigned long pos/*, int dir = CUR*/);
+  virtual bool eof();
+
+  int currentPage() const;
+  int currentOffset() const;
+};
+
+#endif /* AT45DB161DSTREAM_H_ */
